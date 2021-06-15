30b02646c83ecfb98abc460bafcca4fff470fe66
Sumit Ray <sumitrayleo@gmail.com>
2021-06-15 18:33:00 +0530

    Customer Assignment (51)

--- src/main/java/ray/sumit/bbms/msbreweryclient/web/client/BreweryClient.java
+++ src/main/java/ray/sumit/bbms/msbreweryclient/web/client/BreweryClient.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ray.sumit.bbms.msbreweryclient.web.model.BeerDto;
+import ray.sumit.bbms.msbreweryclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -14,6 +15,7 @@ import java.util.UUID;
 public class BreweryClient {
 
     public static final String BEER_PATH_V1 = "/api/v1/beer/";
+    public static final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
     private String apiHost;
 
     private final RestTemplate restTemplate;
@@ -36,6 +38,22 @@ public class BreweryClient {
 
     public void deleteBeerById(UUID beerId) {
         restTemplate.delete(apiHost + BEER_PATH_V1 + beerId.toString());
+    }
+
+    public CustomerDto getCustomerById(UUID randomUUID) {
+        return restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + randomUUID.toString(), CustomerDto.class);
+    }
+
+    public URI saveNewCustomer(CustomerDto customerDto) {
+        return restTemplate.postForLocation(apiHost + CUSTOMER_PATH_V1, customerDto);
+    }
+
+    public void updateExistingCustomer(UUID randomUUID, CustomerDto customerDto) {
+        restTemplate.getForObject(apiHost + CUSTOMER_PATH_V1 + randomUUID.toString(), CustomerDto.class);
+    }
+
+    public void deleteCustomerById(UUID randomUUID) {
+        restTemplate.delete(apiHost + CUSTOMER_PATH_V1 + randomUUID.toString());
     }
 
     public void setApiHost(String apiHost) {
