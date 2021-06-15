d9532dc942a5c78ee4aaf66abd3321565fd10281
Sumit Ray <sumitrayleo@gmail.com>
2021-06-15 18:31:42 +0530

    Customer Assignment (51)

--- src/test/java/ray/sumit/bbms/msbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/ray/sumit/bbms/msbreweryclient/web/client/BreweryClientTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ray.sumit.bbms.msbreweryclient.web.model.BeerDto;
+import ray.sumit.bbms.msbreweryclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -41,9 +42,34 @@ class BreweryClientTest {
 
     @Test
     void deleteExistingBeer() {
-        //given
-        BeerDto beerDto = BeerDto.builder().beerName("Black Label").build();
         client.deleteBeerById(UUID.randomUUID());
+    }
 
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("Sierra Jones").build();
+        URI uri = client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void saveExistingCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("Sierra Jones").build();
+        client.updateExistingCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteExistingCustomer() {
+        client.deleteCustomerById(UUID.randomUUID());
     }
 }
